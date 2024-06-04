35c5af29f62fd14179e1bc5d111cb8c184ba95e0
Matheiu Perdido <matheiuamiel@gmail.com>
2024-06-04 22:38:29 +0800

    add search yap

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -7,7 +7,14 @@ use Illuminate\Http\Request;
 
 class DashboardController extends Controller
 {
-    public function index(){
-        return view('dashboard', ['yaps' => Yap::orderBy('created_at', 'DESC')->paginate(5)]);
+    public function index()
+    {
+
+        $yaps = Yap::orderBy('created_at', 'DESC');
+
+        if (request()->has('search')) {
+            $yaps = $yaps->where('yap', 'like', '%' . request()->get('search', '') . '%');
+        }
+        return view('dashboard', ['yaps' => $yaps->paginate(5)]);
     }
 }
